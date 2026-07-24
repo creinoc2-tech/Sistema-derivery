db89c9265f8448707431797a69a656b0e34a3d32
user <Christian>
2026-07-23 19:09:24 -0500

    Autentificaion

--- frontend/src/routes/_layout/profile.tsx
+++ frontend/src/routes/_layout/profile.tsx
@@ -1,10 +1,24 @@
 import ProfileTemplate from '#/components/templat/store/accounts/profile/profile-template'
-import { createFileRoute } from '@tanstack/react-router'
+import { useUser } from '@clerk/react'
+import { createFileRoute, useNavigate } from '@tanstack/react-router'
+import { useEffect } from 'react'
 
 export const Route = createFileRoute('/_layout/profile')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
+  const { isSignedIn, isLoaded } = useUser()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (isLoaded && !isSignedIn) {
+      navigate({ to: '/' })
+    }
+  }, [isLoaded, isSignedIn, navigate])
+
+  if (!isLoaded) return null
+  if (!isSignedIn) return null
+
   return <ProfileTemplate />
 }
